0e1de588d62084f6d516f661d0795cafaa380a8d
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-08 00:39:44 +0200

    Create MBC_DOCS.py

---
+++ MBC_DOCS.py
@@ -0,0 +1,8 @@
+# mbc_docs.py
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.header(':blue[Pagina Principală]', divider='rainbow')
+st.write(':violet[Bine ați venit la aplicația pentru completarea - Planului de Afaceri! -]')
+
