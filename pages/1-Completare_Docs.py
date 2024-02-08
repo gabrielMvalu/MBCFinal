e2a6bad8dba475fda7d2d39aacda643203de5730
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-08 13:13:46 +0200

    Update 1-Completare_Docs.py

--- pages/1-Completare_Docs.py
+++ pages/1-Completare_Docs.py
@@ -31,8 +31,6 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     uploaded_doc1 = st.file_uploader("Încărcați fișierul Date Solicitate", type=["xlsx"], key="dateSolicitate")
-    st.success("Date Solicitate")
-    
     if uploaded_doc1 is not None:
         datesolicitate_doc = pd.read_excel(uploaded_doc1)
         date_din_xlsx_date_solicitate = extrage_date_solicitate(datesolicitate_doc)
@@ -42,38 +40,34 @@ with col1:
         st.session_state.noua_veche_foi = date_din_xlsx_date_solicitate.get('Activitate', 'Activitate necunoscuta')
         
         firma = date_din_xlsx_date_solicitate.get('Denumirea firmei SRL', 'Firmă necunoscută')
-        
         match = re.search(r'CAEN (\d+)', caen_extras)
         if match:
-            st.session_state.caen_nr_extras_foi = match.group(1)  
+            st.session_state.caen_nr_extras_foi = match.group(1)
         else:
-            st.session_state.caen_nr_extras_foi = None 
+            st.session_state.caen_nr_extras_foi = None
         
-        st.success(f"Vom începe prelucrarea firmei: {firma} cu prelucrarea pe codul CAEN: {st.session_state.caen_nr_extras_foi} - {caen_extras} ")
-
-        document_succes = True  
+        st.success(f"Vom începe prelucrarea firmei: {firma} cu prelucrarea pe codul CAEN: {st.session_state.caen_nr_extras_foi} - {caen_extras}")
+        document_succes = True
 
 with col2:
     if document_succes:
         uploaded_doc2 = st.file_uploader("Încărcați al doilea document", type=["docx"], key="RaportInterogare")
-        st.info("Raport interogare")
-
         if uploaded_doc2 is not None:
             template_doc = Document(uploaded_doc2)
-            st.toast('Incepem procesarea Planului de afaceri', icon='⭐') 
+            st.toast('Incepem procesarea Planului de afaceri', icon='⭐')
             ion = date_din_xlsx_date_solicitate.get('Cod CAEN', 'Cod CAEN necunoscut')
-            st.info(f"Vom începe prelucrarea firmei: {ion} cu prelucrarea pe codul CAEN: {st.session_state.caen_nr_extras_foi} ")
+            st.info(f"Vom începe prelucrarea firmei: {ion} cu prelucrarea pe codul CAEN: {st.session_state.caen_nr_extras_foi}")
             document2_succes = True
+    else:
+        st.error("Vă rugăm să încărcați și să procesați mai întâi documentul din prima coloană.")
 
 with col3:
-    if document2_succes:  # Această condiție presupune că documentul din col2 a fost încărcat și procesat cu succes
+    if document2_succes:
         uploaded_doc3 = st.file_uploader("Încărcați al 3-lea document", type=["xlsx"], key="AnalizaMacheta")
-        st.success("Incepem prelucrarea analizei")
-        
         if uploaded_doc3 is not None:
-            # Asigurați-vă că valorile sunt disponibile în st.session_state înainte de a le utiliza
             caen_nr_extras_foi = st.session_state.get('caen_nr_extras_foi', 'nedefinit')
             judet_foi = st.session_state.get('judet_foi', 'nedefinit')
             noua_veche_foi = st.session_state.get('noua_veche_foi', 'nedefinit')
-
             st.success(f"Vom începe prelucrarea analizei financiare CAEN: {caen_nr_extras_foi} JUDET: {judet_foi} NOUA SAU VECHE: {noua_veche_foi}")
+    else:
+        st.error("Vă rugăm să încărcați și să procesați documentele din primele două coloane.")
