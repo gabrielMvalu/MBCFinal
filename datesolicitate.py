8535d9e49daa06994e15ee4f6a598efe472ace97
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-08 19:39:57 +0200

    Update datesolicitate.py

--- datesolicitate.py
+++ datesolicitate.py
@@ -1,5 +1,62 @@
 import pandas as pd
 
+def extrage_date_suplimentare(judet, caen, tip_activitate):
+    with pd.ExcelFile('./variabile/machetaVariabile.xlsx') as xls:
+        df_judet = pd.read_excel(xls, sheet_name=judet)
+        df_caen = pd.read_excel(xls, sheet_name=caen)
+        df_tip_activitate = pd.read_excel(xls, sheet_name=tip_activitate)
+
+    contributia_proiectului_la_TJ = df_judet.iloc[1, 1]
+    strategii_materiale = df_judet.iloc[2, 1]
+    strategii_reciclare = df_judet.iloc[3, 1]
+    
+    activitate = df_caen.iloc[1, 1] 
+    descriere_utilaj_reciclare = df_caen.iloc[2, 1]
+    lucrari_inovatie = df_caen.iloc[3, 1]
+    lucrari_caen = df_caen.iloc[4, 1]
+    aDNSH = df_caen.iloc[5, 1]
+    cDNSH = df_caen.iloc[6, 1]
+    dDNSH = df_caen.iloc[7, 1]
+    materiale_locale = df_caen.iloc[8, 1]
+    pregatireaTeren = df_caen.iloc[9, 1]
+    reciclareaMaterialelor = df_caen.iloc[10, 1]
+    clientiFirma = df_caen.iloc[11, 1]
+    descriere_serviciu = df_caen.iloc[12, 1]
+    piata_tinta = df_caen.iloc[13, 1]
+
+    crestere_creare = df_tip_activitate.iloc[1, 1]
+    creareActivVizata = df_tip_activitate.iloc[2, 1]
+    dezavantajeConcurentiale = df_tip_activitate.iloc[3, 2]
+    
+    data_foi = {
+        #Variabile din foile ce pot fi nume judet
+        "Contribuția proiectului la tranziția justă": contributia_proiectului_la_TJ,
+        "Strategii materiale": strategii_materiale,
+        "Strategii materiale reciclate": strategii_reciclate,
+
+        #Variabilele din foile ce pot fi nr cod caen
+        "Activitate specifică": activitate,
+        "Inovații în lucrări": lucrari_inovatie,
+        "Lucrări conform codurilor CAEN": lucrari_caen,
+        "Detalii DNSH - A": aDNSH,
+        "Detalii DNSH - C": cDNSH,
+        "Detalii DNSH - D": dDNSH,
+        "Utilizarea materialelor locale": materiale_locale,
+        "Pregătirea terenului pentru lucrări": pregatireaTeren,
+        "Procesul de reciclare a materialelor": reciclareaMaterialelor,
+        "Clienți principali ai firmei": clientiFirma,
+        "Descriere serviciului": descriere_serviciu,
+        "Piata tinta": piata_tinta,
+
+        #Variabile din foi ce pot fi veche sau nou
+        "Creșterea și crearea de noi activități": crestCreare,
+        "Crearea de activități în domeniul vizat": creareActivVizata,
+        "Identificarea dezavantajelor concurențiale": dezavantajeConcurentiale, 
+    }
+
+    return data_foi
+
+
 def extrage_date_solicitate(df):
     firma = df.iloc[2, 2]
     categ_intreprindere = df.iloc[3, 2]
@@ -7,6 +64,7 @@ def extrage_date_solicitate(df):
     tip_investitie = df.iloc[5, 2]
     activitate = df.iloc[6, 2]
     caen = df.iloc[7, 2]
+    nr_caen = df.iloc[43, 2]
     nr_locuri_munca_noi = df.iloc[8, 2]
     judet = df.iloc[9, 2]
     utilaj_dizabilitati = df.iloc[10, 2]
@@ -29,14 +87,21 @@ def extrage_date_solicitate(df):
     flux_tehnologic = df.iloc[38, 2]
     utilajeDNSH = df.iloc[39, 2]
     utilaj_ghidare_descriere = df.iloc[40, 2]
+    dacaTipInvest = df.iloc[41, 2]
+    nrlocmunca30 = df.iloc[49, 2]
+    nrlocmunca20 = df.iloc[50, 2]
+    iso14001 = df.iloc[53, 2]
+
     
     data = {
+        #Variabile extrase din date solicitate completate de consultanti
         "Denumirea firmei SRL": firma, 
         "Categorie întreprindere": categ_intreprindere, 
         "Firme legate": firme_legate,  
         "Tipul investiției": tip_investitie,  
         "Activitate": activitate,
         "Cod CAEN": caen,
+        "Doar nr CAEN": nr_caen,
         "Număr locuri de muncă noi": nr_locuri_munca_noi,
         "Județ": judet,
         "Utilaj pentru persoane cu dizabilități": utilaj_dizabilitati,
@@ -59,7 +124,15 @@ def extrage_date_solicitate(df):
         "Fluxul tehnologic": flux_tehnologic,
         "DNSH pentru utilaje": utilajeDNSH,
         "Descrierea utilaj ghidare": utilaj_ghidare_descriere,
-    
+        "Tipul investitiei": dacaTipInvest,
+        "Procent 30% din total locuri munca nou create": nrlocmunca30,
+        "Procent 30% din total locuri munca nou create": nrlocmunca20,
+        "Daca are sau nu iso14001": iso14001,
     }
+    
+    # Apelarea funcției pentru a extrage datele suplimentare
+    date_suplimentare = extrage_date_suplimentare(judet, nr_caen, tip_activitate)
+    # Actualizarea dicționarului `data` cu datele suplimentare extrase
+    data.update(date_suplimentare)
 
     return data
