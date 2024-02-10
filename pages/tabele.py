64535f2bddddac5ac3904f8ade6de76e2b415205
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-10 18:37:22 +0200

    Update tabele.py

--- pages/tabele.py
+++ pages/tabele.py
@@ -15,16 +15,16 @@ if uploaded_file is not None:
     # Citirea datelor din fișierul Excel încărcat
     df = pd.read_excel(uploaded_file, sheet_name='P. FINANCIAR')
 
-    # Găsirea rândului care conține stop_text în coloana B
-    stop_index = df[df['B'] == stop_text].index.min()  # Presupunem că 'B' este numele coloanei
+    # Găsirea rândului care conține stop_text în coloana B (folosind indexul 1 pentru a accesa coloana B)
+    stop_index = df[df.iloc[:, 1] == stop_text].index.min()
 
     # Verificăm dacă stop_text a fost găsit
     if pd.notna(stop_index):
         # Selectarea datelor de la rândul 5 până la rândul stop_index
         df_filtered = df.iloc[4:stop_index]  # Indexarea începe de la 0, de aceea folosim 4 pentru rândul 5
     else:
-        st.write('Textul de stop nu a fost găsit. Se afișează toate datele până la rândul 35.')
-        df_filtered = df.iloc[35:]  # Selectăm toate datele începând cu rândul 35
+        st.write('Textul de stop nu a fost găsit. Se afișează toate datele începând cu rândul 5.')
+        df_filtered = df.iloc[4:]  # Selectăm toate datele începând cu rândul 5
 
     # Afișarea datelor filtrate în Streamlit
     st.write('Datele filtrate:')
