ef7f5ff6b94840d26afe855487f6d21aed9321d9
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-10 18:45:31 +0200

    Update tabele.py

--- pages/tabele.py
+++ pages/tabele.py
@@ -17,11 +17,11 @@ if uploaded_file is not None:
 
     # Găsirea rândului care conține stop_text în coloana B (folosind indexul 1 pentru a accesa coloana B)
     stop_row = df[df.iloc[:, 1] == stop_text].index.min()
-    st.write(f"Index {stop_row}")
+
     # Verificăm dacă stop_text a fost găsit
     if pd.notna(stop_row):
         # Selectarea datelor de la rândul 5 până la rândul stop_index
-        df_filtered = df.iloc[4:stop_row]  # Indexarea începe de la 0, de aceea folosim 4 pentru rândul 5
+        df_filtered = df.iloc[4:stop_row + 1]  # Indexarea începe de la 0, de aceea folosim 4 pentru rândul 5
     else:
         st.write('Textul de stop nu a fost găsit. Se afișează toate datele începând cu rândul 5.')
         df_filtered = df.iloc[4:]  # Selectăm toate datele începând cu rândul 5
