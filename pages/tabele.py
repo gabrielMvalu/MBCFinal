63c29caf39eb162304ff246b4a991a075cdf8952
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-10 18:27:27 +0200

    Create tabele.py

---
+++ pages/tabele.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+# Titlul aplicației
+st.header(':blue[Prelucrare Tabelara]', divider='rainbow')
+
+# Widget pentru încărcarea fișierului
+uploaded_file = st.file_uploader("Incarca Anexa 3 Macheta Financiara (.xlsx)", type=['xlsx'])
+
+# Verifică dacă a fost încărcat un fișier
+if uploaded_file is not None:
+  
+    # Citirea datelor din fișierul Excel încărcat
+    df = pd.read_excel(uploaded_file)
+
+    # Afișarea datelor în Streamlit
+    st.write('Datele încărcate:')
+    st.dataframe(df)
+  
+else:
+    st.write('Așteptând încărcarea unui fișier Excel...')
