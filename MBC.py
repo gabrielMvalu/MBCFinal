50a95fb73960b37f759b27794d7fa705586de6c3
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-11 22:20:23 +0200

    Update MBC.py

--- MBC.py
+++ MBC.py
@@ -1,5 +1,4 @@
 # mbc_docs.py
-# mbc_docs.py
 from openai import OpenAI
 import streamlit as st
 
@@ -25,6 +24,7 @@ else:
     # Inițializarea clientului OpenAI cu cheia API introdusă
     client = OpenAI(api_key=openai_api_key)
 
+with st.container(height=400):
     # Inițializarea stării sesiunii pentru model și mesaje
     if "openai_model" not in st.session_state:
         st.session_state["openai_model"] = "gpt-4-0125-preview"
