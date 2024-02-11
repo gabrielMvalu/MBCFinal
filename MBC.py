439a9b485896562ba3e303f3ffda33bf8bc4defa
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-11 22:50:20 +0200

    Update MBC.py

--- MBC.py
+++ MBC.py
@@ -1,4 +1,5 @@
 # mbc_docs.py
+# mbc_docs.py
 from openai import OpenAI
 import streamlit as st
 
@@ -7,39 +8,42 @@ st.set_page_config(layout="wide")
 st.header(':blue[Pagina Principală]', divider='rainbow')
 st.write(':violet[Bine ați venit la aplicația pentru completarea - Planului de Afaceri! -]')
 
-with st.expander("ℹ️ Mesaj Informativ ℹ️"):
+
+with st.expander(" ℹ️ Mesaj Informativ ℹ️  "):
     st.write("""
-        Dorim să vă informăm că acest bot se află într-o fază incipientă de dezvoltare. 
-        În acest moment, funcționalitatea este limitată la furnizarea de răspunsuri generale. Ne străduim să îmbunătățim și să extindem capacitățile aplicației. 
-        Vă mulțumim pentru înțelegere și răbdare!
+        Vă informăm că aceast bot se află într-o fază incipientă de dezvoltare. 
+        În acest moment, funcționalitatea este limitată la furnizarea de răspunsuri generale.
     """)
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-
+    
 if not openai_api_key:
     st.error("Vă rugăm să introduceți cheia API OpenAI în bara laterală.")
 else:
+    # Inițializarea clientului OpenAI cu cheia API introdusă
     client = OpenAI(api_key=openai_api_key)
 
+    # Inițializarea stării sesiunii pentru model și mesaje
     if "openai_model" not in st.session_state:
         st.session_state["openai_model"] = "gpt-4-0125-preview"
 
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
-    # Container pentru mesaje cu o înălțime fixă de 250px
-    with st.container(height=250):
-        for message in st.session_state.messages:
-            st.write(message["role"], ": ", message["content"])
+    # Afișarea mesajelor anterioare
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-    # Input pentru mesaj nou de la utilizator plasat la partea de jos a aplicației
-    with st.container():
-        prompt = st.text_input("Adaugati mesajul aici.")
-        if prompt:
-            st.session_state.messages.append({"role": "user", "content": prompt})
+    # Input pentru mesaj nou de la utilizator
+    if prompt := st.chat_input("Adaugati mesajul aici."):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
 
-            # Generarea răspunsului asistentului și afișarea acestuia folosind st.write_stream
+        # Generarea răspunsului asistentului și afișarea acestuia
+        with st.chat_message("assistant"):
             stream = client.chat.completions.create(
                 model=st.session_state["openai_model"],
                 messages=[
@@ -49,4 +53,5 @@ else:
                 stream=True,
             )
             response = st.write_stream(stream)
-            st.session_state.messages.append({"role": "assistant", "content": response})
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
