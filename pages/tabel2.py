7b7530480771ee274b4ed666cd6911ca700742f1
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-11 05:14:34 +0200

    Update tabel2.py

--- pages/tabel2.py
+++ pages/tabel2.py
@@ -92,7 +92,7 @@ def transforma_date_tabel2(df):
                 um.append("buc")
                 cantitate.append(df_filtrat.iloc[i-1, 11])  # Adjust the index as necessary
                 pret_unitar.append(df_filtrat.iloc[i-1, 3])
-                valoare_totala.append(df_filtrat.iloc[i-1, 6])
+                valoare_totala.append(df_filtrat.iloc[i-1, 3] * df_filtrat.iloc[i-1, 11])
                 nr_crt_counter += 1
         
             # Add other specific entries after processing all items
