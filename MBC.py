3fc84b0b54c1a491614e031f0da110a2e5e687fd
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-11 20:58:28 +0200

    Update MBC.py

--- MBC.py
+++ MBC.py
@@ -1,34 +1,35 @@
 # mbc_docs.py
+# mbc_docs.py
 from openai import OpenAI
 import streamlit as st
 
-st.title("ChatGPT-like clone")
+st.set_page_config(layout="wide")
 
-client = OpenAI(api_key=st.secrets["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"])
 
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    
+
+st.header(':blue[Pagina Principală]', divider='rainbow')
+st.write(':violet[Bine ați venit la aplicația pentru completarea - Planului de Afaceri! -]')
+
 
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+    st.session_state["messages"] = [{"role": "assistant", "content": "Cu ce te pot ajuta?"}]
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
 
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Adaugati OpenAI API key pentru a putea continua.")
+        st.stop()
+
+    client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    st.chat_message("user").write(prompt)
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
