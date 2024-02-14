a293cf0a68e97e86988ef341c563d5e9691c97e9
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-14 18:16:59 +0200

    Update pagina_testare.py

--- pages/pagina_testare.py
+++ pages/pagina_testare.py
@@ -7,7 +7,7 @@ stop_text3 = 'Total active necorporale'
 
 def extrage_cheltuieli_buget(df):
     stop_row = df[df.iloc[:, 1] == stop_text].index.min()
-    df_filtrat = df.iloc[3:stop_row, [1, 10]] if pd.notna(stop_row) else df.iloc[3:, [1, 10]]  # Selectează coloanele de interes
+    df_filtrat = df.iloc[3:stop_row, [1, 11]] if pd.notna(stop_row) else df.iloc[3:, [1, 11]]  # Selectează coloanele de interes
     conditii_de_excludere = (df_filtrat.iloc[:, 0] == stop_text) | (df_filtrat.iloc[:, 0] == stop_text2) | (df_filtrat.iloc[:, 0] == stop_text3) | (df_filtrat.iloc[:, 0] == '-') | (df_filtrat.iloc[:, 0] == 0) | df_filtrat.iloc[:, 0].isna()
     df_filtrat = df_filtrat[~conditii_de_excludere]
     return df_filtrat
@@ -25,7 +25,7 @@ if uploaded_file is not None:
 
             # Afișarea valorilor sub forma specificată
             for index, row in df_rezultate.iterrows():
-                st.write(f"({row.iloc[0]} - {row.iloc[1]} buc.)")
+                st.write(f"{row.iloc[0]} - {row.iloc[1]} buc.")
         else:
             st.error("Nu s-au găsit date valide în foaia 'P. FINANCIAR'.")
     except ValueError as e:
