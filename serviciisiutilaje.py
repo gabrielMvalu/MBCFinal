81c758496a41d8b74b8fa2e2b31c452fa611b7c1
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-14 21:48:59 +0200

    Update serviciisiutilaje.py

--- serviciisiutilaje.py
+++ serviciisiutilaje.py
@@ -16,11 +16,11 @@ def extrage_pozitii(df_financiar):
 
 def coreleaza_date(date_financiar):
     df_amortizare = pd.read_excel('./descrieriUtilaje/utilaje.xlsx', sheet_name='amortizare')
-    df_utilaje = pd.read_excel('./descrieriUtilaje/utilaje1.xlsx', sheet_name='utilajeservicii')
+    # df_utilaje = pd.read_excel('./descrieriUtilaje/utilaje1.xlsx', sheet_name='utilajeservicii')
     df_descriere = pd.read_excel(f'./descrieriUtilaje/{st.session_state.codCAEN}.xlsx', sheet_name='utilajedescriere')
     
     amortizare_data = {}
-    servicii_data = {}
+    # servicii_data = {}
     descriere_data = {}
 
     for index, row in df_amortizare.iterrows():
@@ -30,10 +30,10 @@ def coreleaza_date(date_financiar):
             descriere = ' '.join(str(row.iloc[3]).strip().split()) if row.iloc[3] else ''
             amortizare_data[nume] = (cod, descriere)
 
-    for index, row in df_utilaje.iterrows():
-        if row.iloc[1]:
-            nume = ' '.join(re.sub(r'\d+$', '', str(row.iloc[1])).strip().split()).lower()
-            servicii_data[nume] = nume
+    # for index, row in df_utilaje.iterrows():
+    #     if row.iloc[1]:
+    #         nume = ' '.join(re.sub(r'\d+$', '', str(row.iloc[1])).strip().split()).lower()
+    #         servicii_data[nume] = nume
 
     for index, row in df_descriere.iterrows():
         if row.iloc[1]:
@@ -42,7 +42,7 @@ def coreleaza_date(date_financiar):
             descriere_data[nume] = descriere
 
     rezultate_corelate = []
-    rezultate_corelate1 = []
+    # rezultate_corelate1 = []
     rezultate_corelate2 = []
 
     for nume, cantitate in date_financiar:
@@ -51,12 +51,12 @@ def coreleaza_date(date_financiar):
             cod, descriere = amortizare_data[nume_curat]
             rezultat = f"{nume}, {cantitate} buc., ce aparține clasei {cod} {descriere}, conform HG 2139/2004"
             rezultate_corelate.append((nume, cantitate, rezultat))
-        if nume_curat in servicii_data:
-            rezultat1 = f"{nume}, {cantitate} buc"
-            rezultate_corelate1.append((nume, cantitate, rezultat1))
+        # if nume_curat in servicii_data:
+        #     rezultat1 = f"{nume}, {cantitate} buc"
+        #     rezultate_corelate1.append((nume, cantitate, rezultat1))
         if nume_curat in descriere_data:
             descriere = descriere_data[nume_curat]
             rezultat2 = f"{descriere}"
             rezultate_corelate2.append((nume, cantitate, rezultat2))
 
-    return rezultate_corelate, rezultate_corelate1, rezultate_corelate2
+    return rezultate_corelate, rezultate_corelate2
