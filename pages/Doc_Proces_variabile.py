8ce9e04cc4074eea6fc989ccb19e6652139b9ae1
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-03-06 00:35:57 +0200

    Update Doc_Proces_variabile.py

--- pages/Doc_Proces_variabile.py
+++ pages/Doc_Proces_variabile.py
@@ -1,31 +1,25 @@
 import streamlit as st
 from docx import Document
-from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.shared import Pt
 import io
 
-def ajusteaza_alinierea(document):
-    for paragraph in document.paragraphs:
-        if paragraph.alignment == WD_ALIGN_PARAGRAPH.JUSTIFY:
-            paragraph.alignment = WD_ALIGN_PARAGRAPH.LEFT
-        elif paragraph.alignment == WD_ALIGN_PARAGRAPH.LEFT:
-            paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
-
-    for table in document.tables:
-        for row in table.rows:
-            for cell in row.cells:
-                for paragraph in cell.paragraphs:
-                    if paragraph.alignment == WD_ALIGN_PARAGRAPH.JUSTIFY:
-                        paragraph.alignment = WD_ALIGN_PARAGRAPH.LEFT
-                    elif paragraph.alignment == WD_ALIGN_PARAGRAPH.LEFT:
-                        paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+def adauga_text_la_inceput(document, text="A"):
+    # Crează un nou paragraf la începutul documentului
+    paragraph = document.paragraphs[0]._element  # Accesează elementul XML al primului paragraf
+    p = document.add_paragraph()  # Creează un nou paragraf
+    p.add_run(text)  # Adaugă textul specificat în noul paragraf
+    p_font = p.runs[0].font
+    p_font.size = Pt(1)  # Setează dimensiunea fontului la 1 pentru a fi cât mai mic vizual
+    p_element = p._element
+    paragraph.addprevious(p_element)  # Adaugă noul paragraf înaintea primului paragraf existent
 
 def main():
-    st.header("Ajustare Aliniere Document")
+    st.header("Adaugă Text la Începutul Documentului")
 
     uploaded_file = st.file_uploader("Încărcați documentul DOCX aici:", type="docx")
     if uploaded_file is not None:
         document = Document(uploaded_file)
-        ajusteaza_alinierea(document)
+        adauga_text_la_inceput(document)
         
         # Salvăm documentul modificat într-un buffer
         buffer = io.BytesIO()
@@ -39,3 +33,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
