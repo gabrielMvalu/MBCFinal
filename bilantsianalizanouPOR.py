2755d56c8130ca903c16faa70667a885a338bad7
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-04-29 13:14:14 +0300

    Update bilantsianalizanouPOR.py

--- bilantsianalizanouPOR.py
+++ bilantsianalizanouPOR.py
@@ -3,24 +3,24 @@ import pandas as pd
 import streamlit as st
 
 def extrage_date_bilant(df):
-    cpa20 = f"{df.iloc[99, 1]:.2f}"
-    cpa21 = f"{df.iloc[99, 2]:.2f}"
-    cpa22 = f"{df.iloc[99, 3]:.2f}"
+    cpa21 = f"{df.iloc[99, 1]:.2f}"
+    cpa22 = f"{df.iloc[99, 2]:.2f}"
+    cpa23 = f"{df.iloc[99, 3]:.2f}"
     
     data = {
-        "Capitalul propriu al actionarilor 2020": cpa20, 
-        "Capitalul propriu al actionarilor 2021": cpa21,
-        "Capitalul propriu al actionarilor 2022": cpa22
+        "Capitalul propriu al actionarilor 2020": cpa21, 
+        "Capitalul propriu al actionarilor 2021": cpa22,
+        "Capitalul propriu al actionarilor 2022": cpa23
     }
     return data
 
 def extrage_date_contpp(df1):
-    ca20 = f"{df1.iloc[4, 1]:.2f}"
-    ca21 = f"{df1.iloc[4, 2]:.2f}"
-    ca22 = f"{df1.iloc[4, 3]:.2f}"
-    vt20 = f"{df1.iloc[55, 1]:.2f}"
-    vt21 = f"{df1.iloc[55, 2]:.2f}"
-    vt22 = f"{df1.iloc[55, 3]:.2f}"
+    ca21 = f"{df1.iloc[4, 1]:.2f}"
+    ca22 = f"{df1.iloc[4, 2]:.2f}"
+    ca23 = f"{df1.iloc[4, 3]:.2f}"
+    vt21 = f"{df1.iloc[55, 1]:.2f}"
+    vt22 = f"{df1.iloc[55, 2]:.2f}"
+    vt23 = f"{df1.iloc[55, 3]:.2f}"
 
     pfexpl = f"{df1.iloc[33, 3]:.2f}"
 
@@ -30,26 +30,26 @@ def extrage_date_contpp(df1):
         pc = "N/A" 
     
     if df1.iloc[4, 1] > df1.iloc[4, 2] and df1.iloc[4, 1] > df1.iloc[4, 3]:
-        camax = 2020
-    elif df1.iloc[4, 2] > df1.iloc[4, 1] and df1.iloc[4, 2] > df1.iloc[4, 3]:
         camax = 2021
-    else:
+    elif df1.iloc[4, 2] > df1.iloc[4, 1] and df1.iloc[4, 2] > df1.iloc[4, 3]:
         camax = 2022
+    else:
+        camax = 2023
     
-    re20 = f"{df1.iloc[31, 1]:.2f}" if df1.iloc[31, 1] > 0 else f"{df1.iloc[32, 1]:.2f}"
-    re21 = f"{df1.iloc[31, 2]:.2f}" if df1.iloc[31, 2] > 0 else f"{df1.iloc[32, 2]:.2f}"
-    re22 = f"{df1.iloc[31, 3]:.2f}" if df1.iloc[31, 3] > 0 else f"{df1.iloc[32, 3]:.2f}"
+    re21 = f"{df1.iloc[31, 1]:.2f}" if df1.iloc[31, 1] > 0 else f"{df1.iloc[32, 1]:.2f}"
+    re22 = f"{df1.iloc[31, 2]:.2f}" if df1.iloc[31, 2] > 0 else f"{df1.iloc[32, 2]:.2f}"
+    re23 = f"{df1.iloc[31, 3]:.2f}" if df1.iloc[31, 3] > 0 else f"{df1.iloc[32, 3]:.2f}"
     
     data = {
-        "Cifra de afaceri 2020": ca20, 
-        "Cifra de afaceri 2021": ca21,
-        "Cifra de afaceri 2022": ca22,
-        "Venituri totale 2020": vt20, 
-        "Venituri totale 2021": vt21,
-        "Venituri totale 2022": vt22,
-        "Rezultat al exercitiului 2020": re20, 
-        "Rezultat al exercitiului 2021": re21,
-        "Rezultat al exercitiului 2022": re22,  
+        "Cifra de afaceri 2020": ca21, 
+        "Cifra de afaceri 2021": ca22,
+        "Cifra de afaceri 2022": ca23,
+        "Venituri totale 2020": vt21, 
+        "Venituri totale 2021": vt22,
+        "Venituri totale 2022": vt23,
+        "Rezultat al exercitiului 2020": re21, 
+        "Rezultat al exercitiului 2021": re22,
+        "Rezultat al exercitiului 2022": re23,  
         "Anul cu cea mai mare cifra de afaceri": camax, 
         "Procent crestere": pc,
         
@@ -58,41 +58,41 @@ def extrage_date_contpp(df1):
     return data
 
 def extrage_indicatori_financiari(df2):
-    rs20 = f"{df2.iloc[89, 1]:.2f}"  
-    rs21 = f"{df2.iloc[89, 2]:.2f}"
-    rs22 = f"{df2.iloc[89, 3]:.2f}"
+    rs21 = f"{df2.iloc[89, 1]:.2f}"  
+    rs22 = f"{df2.iloc[89, 2]:.2f}"
+    rs23 = f"{df2.iloc[89, 3]:.2f}"
     
-    gdi20 = f"{df2.iloc[94, 1]:.0%}"
-    gdi21 = f"{df2.iloc[94, 2]:.0%}"
-    gdi22 = f"{df2.iloc[94, 3]:.0%}"
+    gdi21 = f"{df2.iloc[94, 1]:.0%}"
+    gdi22 = f"{df2.iloc[94, 2]:.0%}"
+    gdi23 = f"{df2.iloc[94, 3]:.0%}"
 
-    roa20 = f"{df2.iloc[43, 1]:.0%}" if isinstance(df2.iloc[43, 1], (int, float)) else "nu se calculeaza"
-    roa21 = f"{df2.iloc[43, 2]:.0%}" if isinstance(df2.iloc[43, 2], (int, float)) else "nu se calculeaza"
-    roa22 = f"{df2.iloc[43, 3]:.0%}" if isinstance(df2.iloc[43, 3], (int, float)) else "nu se calculeaza"
+    roa21 = f"{df2.iloc[43, 1]:.0%}" if isinstance(df2.iloc[43, 1], (int, float)) else "nu se calculeaza"
+    roa22 = f"{df2.iloc[43, 2]:.0%}" if isinstance(df2.iloc[43, 2], (int, float)) else "nu se calculeaza"
+    roa23 = f"{df2.iloc[43, 3]:.0%}" if isinstance(df2.iloc[43, 3], (int, float)) else "nu se calculeaza"
 
-    roe20 = f"{df2.iloc[47, 1]:.0%}" if isinstance(df2.iloc[47, 1], (int, float)) else "nu se calculeaza"
-    roe21 = f"{df2.iloc[47, 2]:.0%}" if isinstance(df2.iloc[47, 2], (int, float)) else "nu se calculeaza"
-    roe22 = f"{df2.iloc[47, 3]:.0%}" if isinstance(df2.iloc[47, 3], (int, float)) else "nu se calculeaza"
+    roe21 = f"{df2.iloc[47, 1]:.0%}" if isinstance(df2.iloc[47, 1], (int, float)) else "nu se calculeaza"
+    roe22 = f"{df2.iloc[47, 2]:.0%}" if isinstance(df2.iloc[47, 2], (int, float)) else "nu se calculeaza"
+    roe23 = f"{df2.iloc[47, 3]:.0%}" if isinstance(df2.iloc[47, 3], (int, float)) else "nu se calculeaza"
 
     
     #adaugare dupa schimbari din 16.feb 
-    rpe22 = f"{df2.iloc[32, 3]:.2%}"
+    rpe23 = f"{df2.iloc[32, 3]:.2%}"
 
     data = {
-        "Rata solvabilitatii generale 2020": rs20, 
-        "Rata solvabilitatii generale 2021": rs21,
-        "Rata solvabilitatii generale 2022": rs22,
-        "Gradul de indatorare pe termen scurt 2020": gdi20, 
-        "Gradul de indatorare pe termen scurt 2021": gdi21,
-        "Gradul de indatorare pe termen scurt 2022": gdi22,
-        "Rentabilitatea activelor (ROA) 2020": roa20, 
-        "Rentabilitatea activelor (ROA) 2021": roa21,
-        "Rentabilitatea activelor (ROA) 2022": roa22,
-        "Rentabilitatea capitalului propriu (ROE) 2020": roe20, 
-        "Rentabilitatea capitalului propriu (ROE) 2021": roe21,
-        "Rentabilitatea capitalului propriu (ROE) 2022": roe22,
+        "Rata solvabilitatii generale 2020": rs21, 
+        "Rata solvabilitatii generale 2021": rs22,
+        "Rata solvabilitatii generale 2022": rs23,
+        "Gradul de indatorare pe termen scurt 2020": gdi21, 
+        "Gradul de indatorare pe termen scurt 2021": gdi22,
+        "Gradul de indatorare pe termen scurt 2022": gdi23,
+        "Rentabilitatea activelor (ROA) 2020": roa21, 
+        "Rentabilitatea activelor (ROA) 2021": roa22,
+        "Rentabilitatea activelor (ROA) 2022": roa23,
+        "Rentabilitatea capitalului propriu (ROE) 2020": roe21, 
+        "Rentabilitatea capitalului propriu (ROE) 2021": roe22,
+        "Rentabilitatea capitalului propriu (ROE) 2022": roe23,
         #adaugare  schimbari din 16.feb 
-        "Rata profitului din exploatare RPE22": rpe22
+        "Rata profitului din exploatare RPE22": rpe23
     }
 
     return data
